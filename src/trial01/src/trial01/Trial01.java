817cca5ed71752072db48c80b95da94e4ac1b389
jingkie07 <jingkiealipuyo68@gmail.com>
2025-08-12 12:12:19 +0800

    lisod

---
+++ src/trial01/src/trial01/Trial01.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trial01;
+
+/**
+ *
+ * @author Jingkie
+ */
+public class Trial01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("practice ni");
+    }
+    
+}
